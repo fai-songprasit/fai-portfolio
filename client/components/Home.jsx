e7ab81cfb6501aa6e878b0213514fd98a4569a1c
fai-songprasit <prapawan.songprasit@gmail.com>
2020-07-11 17:34:56 +1200

    change stateful components into function component where state is not needed

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,41 +1,38 @@
 import React from 'react'
 
-//include a nav bar somewhere in here!
-class Home extends React.Component {
-    render() {
-        return (
-            <>
-            <h2>Meet and Greet</h2>
-                <p><strong>
-                    Hello and Welcome! 
-                </strong></p>
-                <p>
-                    If you're here then you're probably curious about what I 
-                    can do as a developer. You will find that the contents 
-                    of this page has been ordered chronologically across the 
-                    Web-Dev saga of my life, which began only recently, and 
-                    will continue to grow as I expand into this industry.
-                </p>
-                <p>
-                    Having been in the hospitality industry for most of my working 
-                    career, it was safe to say that I have had next to no experience 
-                    with programming prior to EDA. In hindsight, I wish I had made 
-                    the switch sooner as I didn't realise how much I was going to 
-                    enjoy the challenge and how rewarding and gratifying it would be 
-                    have all the tests pass and have created something myself 
-                    completely from scratch.
-                </p>
-                <p>
-                    So without further ado, allow me to introduce 
-                    myself.
-                </p>
-                <p>
-                    My name is Fai Songprasit and I am a Junior full-stack 
-                    web developer.
-                </p>    
-            </>
-        )
-    }
+const Home = () => {
+    return (
+        <>
+        <h2>Meet and Greet</h2>
+            <p><strong>
+                Hello and Welcome! 
+            </strong></p>
+            <p>
+                If you're here then you're probably curious about what I 
+                can do as a developer. You will find that the contents 
+                of this page has been ordered chronologically across the 
+                Web-Dev saga of my life, which began only recently, and 
+                will continue to grow as I expand into this industry.
+            </p>
+            <p>
+                Having been in the hospitality industry for most of my working 
+                career, it was safe to say that I have had next to no experience 
+                with programming prior to EDA. In hindsight, I wish I had made 
+                the switch sooner as I didn't realise how much I was going to 
+                enjoy the challenge and how rewarding and gratifying it would be 
+                have all the tests pass and have created something myself 
+                completely from scratch.
+            </p>
+            <p>
+                So without further ado, allow me to introduce 
+                myself.
+            </p>
+            <p>
+                My name is Fai Songprasit and I am a Junior full-stack 
+                web developer.
+            </p>    
+        </>
+    )
 }
 
 export default Home
--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,28 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-class NavBar extends React.Component {
-    state = {
-        showBurger: false,
-      }
-    
-      toggleBurger = () => {
-        this.setState({
-          showBurger: !this.state.showBurger
-        })
-      }
-
-      render() {
-        const {showBurger} = this.state
-          return (
-            <div className='navbar'>
-                <Link to='/home' className="nav" onClick={this.toggleBurger}>Home</Link>
-                <Link to='/eda' className="nav" onClick={this.toggleBurger}>EDA-Foundations</Link>
-                <Link to='/bootcamp' className="nav" onClick={this.toggleBurger}>EDA-Bootcamp</Link>
-                <Link to='/solo' className="nav" onClick={this.toggleBurger}>Solo Projects</Link>
-            </div>
-        )
-    }
+const NavBar = () => {
+    return (
+        <div className='navbar'>
+            <Link to='/home' className="nav">Home</Link>
+            <Link to='/eda' className="nav">EDA-Foundations</Link>
+            <Link to='/bootcamp' className="nav">EDA-Bootcamp</Link>
+            <Link to='/solo' className="nav">Solo Projects</Link>
+        </div>
+    )
 }
 
 export default NavBar
